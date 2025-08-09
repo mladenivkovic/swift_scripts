786f31591db7ca7054fc925cf93c8efcbfbf2dcc
Mladen Ivkovic <mivkov@protonmail.com>
2025-08-09 23:17:09 +0100

    print out average step time too

--- getTaskRuntime.py
+++ getTaskRuntime.py
@@ -34,18 +34,22 @@ if args.skip_step_zero:
     data = data[1:]
 
 time_total = data[:, 0].sum()
+time_avg = data[:, 0].mean()
 time_min = data[:, 0].min()
 time_max = data[:, 0].max()
 
 deadtime_total = data[:, 1].sum()
+deadtime_avg = data[:, 1].mean()
 deadtime_min = data[:, 1].min()
 deadtime_max = data[:, 1].max()
 
 print("Time [s]:")
 print(f"  Total: {time_total *1e-3:12.3f}")
+print(f"  Avg:   {time_avg *1e-3:12.3f}")
 print(f"  Min:   {time_min *1e-3:12.3f}")
 print(f"  Max:   {time_max *1e-3:12.3f}")
 print("Deadtime [s]:")
 print(f"  Total: {deadtime_total *1e-3:12.3f}")
+print(f"  Avg:   {deadtime_avg *1e-3:12.3f}")
 print(f"  Min:   {deadtime_min *1e-3:12.3f}")
 print(f"  Max:   {deadtime_max *1e-3:12.3f}")
