47bedfb116547fa2ff173b4458239616de2e42f8
Mladen Ivkovic <mivkov@protonmail.com>
2025-12-24 00:00:54 +0100

    added option to plot by operation

--- plotGPUtimersPaper.py
+++ plotGPUtimersPaper.py
@@ -28,6 +28,8 @@ parser = argparse.ArgumentParser(
     plot the outputs of the timers for a paper.
     Names of node directories and experiment directory names are hardcoded in
     this script.
+    By default, it makes 3 plots: One for each task subtype.
+    Use -o to plot for each operation (pack, unpack, launch) per plot instead.
     """,
     epilog="""
     To get timers output with swift, first configure with
@@ -42,6 +44,14 @@ parser.add_argument(
     help="Include the zeroth step in the timing averages",
 )
 
+parser.add_argument(
+    "-o",
+    "--by-operation",
+    action="store_true",
+    help="Plot times by operation, not by task subtype",
+)
+
+
 
 args = parser.parse_args()
 
@@ -50,7 +60,65 @@ first_index = 1
 if args.include_step_zero:
     first_index = 0
 
+by_operation = args.by_operation
 
+
+# Read in data
+
+results = {}
+
+for node in node_dirs:
+
+    results[node] = {}
+
+    for experiment in experiment_dirs:
+
+        fullfile = os.path.join(node, experiment, timer_file)
+
+        cols_to_use = []
+        # add +1: 0th column in `timer_names` is "step"
+        cols_to_use.append(timer_names.index("gpu_pack_density") + 1)
+        cols_to_use.append(timer_names.index("gpu_pack_gradient") + 1)
+        cols_to_use.append(timer_names.index("gpu_pack_force") + 1)
+        cols_to_use.append(timer_names.index("gpu_unpack_density") + 1)
+        cols_to_use.append(timer_names.index("gpu_unpack_gradient") + 1)
+        cols_to_use.append(timer_names.index("gpu_unpack_force") + 1)
+        cols_to_use.append(timer_names.index("gpu_launch_density") + 1)
+        cols_to_use.append(timer_names.index("gpu_launch_gradient") + 1)
+        cols_to_use.append(timer_names.index("gpu_launch_force") + 1)
+        cols_to_use.append(timer_names.index("gpu_self_recurse") + 1)
+        cols_to_use.append(timer_names.index("gpu_pair_recurse") + 1)
+
+        data = np.loadtxt(fullfile, usecols=cols_to_use)
+
+        density = {
+                "pack": data[first_index:, 0].sum(),
+                "unpack": data[first_index:, 3].sum(),
+                "launch": data[first_index:, 6].sum(),
+                }
+        gradient = {
+                "pack": data[first_index:, 1].sum(),
+                "unpack": data[first_index:, 4].sum(),
+                "launch": data[first_index:, 7].sum(),
+                }
+        force = {
+                "pack": data[first_index:, 2].sum(),
+                "unpack": data[first_index:, 5].sum(),
+                "launch": data[first_index:, 8].sum(),
+                }
+
+        density["total"] = density["pack"] + density["unpack"] + density["launch"]
+        gradient["total"] = gradient["pack"] + gradient["unpack"] + gradient["launch"]
+        force["total"] = force["pack"] + force["unpack"] + force["launch"]
+
+
+        experiment_result = {
+                "density": density,
+                "gradient": gradient,
+                "force": force
+                }
+
+        results[node][experiment] = experiment_result
 
 
 
@@ -67,18 +135,20 @@ nbars = len(node_dirs) + len(experiment_dirs)
 x = np.arange(3)
 width = 1. / (nbars + 1)
 
+# bar/ticks are centered on bar at location of x, so it starts already
+# shifted by width/2.
+xticks = x + 0.5 * (nbars - 1) * width
 
-def plot_bars(ax, pack_timer, launch_timer, unpack_timer, title):
+
+def plot_by_task_subtype(ax, task_type, title):
     """
-    Plots the data of a single task type onto an axis.
+    Plots the data of a single task (sub)type onto an axis.
 
     Parameters
     ----------
 
     ax: axis object to plot onto
-    pack_timer: string of timer name for packing timer
-    launch_timer: string of timer name for launch timer
-    unpack_timer: string of timer name for unpacking timer
+    task_type: task type (density, force, gradient) to plot
     title: title to add to the axis
     """
 
@@ -86,21 +156,10 @@ def plot_bars(ax, pack_timer, launch_timer, unpack_timer, title):
     for node in node_dirs:
         for experiment in experiment_dirs:
 
-            fullfile = os.path.join(node, experiment, timer_file)
-
-            # Load data
-            cols_to_use = []
-            # add + 1: timer 0 is "step"
-            cols_to_use.append(timer_names.index(pack_timer) + 1)
-            cols_to_use.append(timer_names.index(launch_timer) + 1)
-            cols_to_use.append(timer_names.index(unpack_timer) + 1)
-            data = np.loadtxt(fullfile, usecols=cols_to_use)
-
-            pack = data[first_index:, 0].sum()
-            launch = data[first_index:, 1].sum()
-            unpack = data[first_index:, 2].sum()
-
-            total = pack + launch + unpack
+            pack = results[node][experiment][task_type]["pack"]
+            unpack = results[node][experiment][task_type]["unpack"]
+            launch = results[node][experiment][task_type]["launch"]
+            total = results[node][experiment][task_type]["total"]
 
             color = "C" + str(index)
             offset = index * width
@@ -117,20 +176,75 @@ def plot_bars(ax, pack_timer, launch_timer, unpack_timer, title):
             index += 1
 
     ax.set_title(title)
+    ax.set_xticks(xticks, ["pack", "launch", "unpack"])
 
     return
 
 
-plot_bars(ax1, "gpu_pack_density", "gpu_launch_density", "gpu_unpack_density", "Density")
-plot_bars(ax2, "gpu_pack_gradient", "gpu_launch_gradient", "gpu_unpack_gradient", "Gradient")
-plot_bars(ax3, "gpu_pack_force", "gpu_launch_force", "gpu_unpack_force", "Force")
+
+def plot_by_operation(ax, operation, title):
+    """
+    Plots the data of a single operation onto an axis.
+
+    Parameters
+    ----------
+
+    ax: axis object to plot onto
+    operation: string of operation name (pack, unpack, launch) for packing
+    timer
+    title: title to add to the axis
+    """
+
+    index = 0
+    for node in node_dirs:
+        for experiment in experiment_dirs:
+
+            density = results[node][experiment]["density"][operation]
+            gradient = results[node][experiment]["gradient"][operation]
+            force = results[node][experiment]["force"][operation]
+
+            total_density = results[node][experiment]["density"]["total"]
+            total_gradient = results[node][experiment]["gradient"]["total"]
+            total_force = results[node][experiment]["force"]["total"]
+
+            color = "C" + str(index)
+            offset = index * width
+            label =  node + "/" + experiment
+            pltkwargs = {
+                    "color": color,
+                    "width": width,
+                    }
+
+            ax.bar(x[0] + offset, density / total_density, **pltkwargs, label=label)
+            ax.bar(x[1] + offset, gradient / total_gradient, **pltkwargs)
+            ax.bar(x[2] + offset, force / total_force, **pltkwargs)
+
+            index += 1
+
+    ax.set_title(title)
+    ax.set_xticks(xticks, ["density", "gradient", "force"])
+    ax.set_ylim([0, 1.0])
+
+    return
 
 
-# bar/ticks are centered on bar at location of x, so it starts already
-# shifted by width/2.
-xticks = x + 0.5 * (nbars - 1) * width
+
+
+
+if by_operation:
+    plot_by_operation(ax1, "pack", "Pack")
+    plot_by_operation(ax2, "launch", "Launch")
+    plot_by_operation(ax3, "unpack", "Unpack")
+
+
+
+else:
+    plot_by_task_subtype(ax1, "density", "Density")
+    plot_by_task_subtype(ax2, "gradient", "Gradient")
+    plot_by_task_subtype(ax3, "force", "Force")
+
+
 for ax in fig.axes:
-    ax.set_xticks(xticks, ["pack", "launch", "unpack"])
     ax.legend()
     ax.grid()
 ax1.set_ylabel("Fraction of total time for offload")
@@ -140,5 +254,10 @@ ax1.set_ylabel("Fraction of total time for offload")
 plt.tight_layout() #rect=(0.05, 0.05, 0.95, 0.95))
 
 #  plt.show()
-plt.savefig("gpu_timers_paper.pdf")
-print("saved gpu_timers_paper.pdf")
+figname="gpu_timers_paper.pdf"
+if by_operation:
+    figname="gpu_timers_paper_by_operation.pdf"
+
+plt.savefig(figname)
+print(figname)
+
